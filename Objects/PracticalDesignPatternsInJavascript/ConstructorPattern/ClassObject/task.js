bba56df41424e91a2521f0f3cefea00860f0cf5a
Sebastiano Gaudeano <sebastiano.gaudeano@dmz.dom>
2017-09-29 17:25:11 +0200

    Add Task Example in a more OOP Way with ES5

---
+++ Objects/PracticalDesignPatternsInJavascript/ConstructorPattern/ClassObject/task.js
@@ -0,0 +1,20 @@
+'use strict';
+
+class Task {
+    constructor(name){
+        this.name = name;
+        this.completed = false;
+    }
+
+    complete(){
+        console.log('Completing task: ' + this.name);
+        this.completed = true;
+    
+    }
+
+    save(){
+        console.log('Saving Task: ' + this.name);        
+    }
+}
+
+module.exports = Task;
